d73a82b69ffc93c0c96bc9fcce74c89a4f190085
YukiYukiVirtual <37247312+YukiYukiVirtual@users.noreply.github.com>
2020-05-04 00:04:39 +0900

    ゆれ

---
+++ MyAvatarScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAvatarScript : MonoBehaviour
+{
+	public GameObject avatar;
+	
+	public float T=1;
+	public float F=1;
+	
+	public bool move = true;
+	
+	private bool ___firstonly = true; 
+	
+	void Start()
+	{
+		if(!avatar)
+		{
+			Debug.Log("Avatar is null");
+		}
+	}
+	
+    void Update()
+    {
+		if(!avatar)
+		{
+			return;
+		}
+		Transform transform = avatar.transform;
+		
+		{
+			transform.localPosition = this.transform.localPosition;
+			transform.localRotation = this.transform.localRotation;
+		}
+		
+		if(move){
+			float pos = Mathf.Sin(Time.time * F) * T;
+			transform.localPosition = this.transform.localPosition + new Vector3(0, pos, 0);
+		}
+		
+    }
+}
